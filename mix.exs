729cedd3ccff62ccc719e7ced646852875d379c5
Blake Williams <blake@blakewilliams.me>
2015-02-26 12:06:36 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Pact.Mixfile do
 
   def project do
     [app: :pact,
-     version: "0.0.2",
+     version: "0.1.0",
      elixir: "~> 1.0",
      deps: deps,
      description: description,
